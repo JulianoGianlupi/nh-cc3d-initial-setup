927ca29764ed129636f2a51bf24a94bf375c2242
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2019-04-17 10:59:27 -0400

    Update compucell3d_initi_set.sh

--- bin/compucell3d_initi_set.sh
+++ bin/compucell3d_initi_set.sh
@@ -39,7 +39,7 @@ export SOSLIB_PATH=${PREFIX_CC3D}/examplesSoslib
 
 echo "CompuCell3D - version $COMPUCELL3D_MAJOR_VERSION.$COMPUCELL3D_MINOR_VERSION.$COMPUCELL3D_BUILD_VERSION"
 
-${PYTHON_EXEC} ${PREFIX_CC3D}/player5/compucell3d.pyw -i ${USER_DEMO_PATH}/$CC3D_DEMO_FILENAME -o ${USER_DEMO_PATH}/output/ -f 50 --currentDir=${PWD}
+${PYTHON_EXEC} ${PREFIX_CC3D}/player5/compucell3d.pyw --currentDir=${PWD}
 exit_code=$?
 
 exit ${exit_code}
